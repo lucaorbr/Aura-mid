92fbf6c474a79859d60cb7b54a386d6945e2010c
LUCAS RIBEIRO DE OLIVEIRA <89263980+lucaorbr@users.noreply.github.com>
2022-06-09 22:51:03 -0300

    Commit do Projeto

---
+++ aluramidi/main.js
@@ -0,0 +1,56 @@
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio).play();
+
+    if (elemento != null && elemento.localName === 'audio' ){
+        elemento.play();
+    }
+    else {
+        console.log('Elemento não encontrado');
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+// USANDO FOR 
+for (let contador =0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function(evento){
+
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+ }
+
+/*AQUI USANDO WHILE = ENQUANTO, MUDAMOS PARA "FOR" PARA FICAR MELHOR O CÓDIGO.
+let contador = 0;
+ 
+while ( contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    //template string
+    const idAudio = `#som_${instrumento}`;
+    
+    tecla.onclick = function () {
+        tocaSom (idAudio)
+    };
+
+    contador = contador + 1;
+
+    console.log(contador);
+    */
